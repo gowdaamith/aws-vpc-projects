def27a943a842d64c014dadcd0fc01f410cf380d
gowdaamith <amithgowda1772@gmail.com>
2026-04-26 05:12:11 +0000

    correction

--- Zero-trust-network/aws-infrastructure/environment/prod/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "cloudbite-terraform-state-prod"
-    key            = "networking/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-locks"
-  }
-}
--- Zero-trust-network/aws-infrastructure/environment/prod/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "production_vpc_id" {
-  value = module.networking.vpc_id
-}
